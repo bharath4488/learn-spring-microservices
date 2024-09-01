6cff52508fc4f5a19fc16f9fadcccd29852dabc7
Bharath <bharath.tupakula@soprasteria.com>
2024-09-01 12:12:05 +0530

    new

--- School/src/main/java/com/learn/School/response/StudentClient.java
+++ School/src/main/java/com/learn/School/response/StudentClient.java
@@ -1,16 +1,27 @@
 package com.learn.School.response;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @FeignClient(name = "student-service", url = "${application.config.students-url}")
 public interface StudentClient {
 
     @GetMapping("/school/{school-id}")
+    @CircuitBreaker(name = "findCommentsByPostIdCircuitBreaker", fallbackMethod = "findCommentsByPostIdFallback")
     List<Student> findAllBySchool(@PathVariable("school-id") Long schoolId);
+
+    default List<Student> findCommentsByPostIdFallback(Exception exception) {
+        System.out.println("\"circuit breaker default method\"");
+        return Arrays.asList();
+    }
 
 }
---
+++ Student/src/test/java/com/learn/Student/StudentServiceTest.java
@@ -0,0 +1,25 @@
+package com.learn.Student;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class StudentServiceTest {
+
+    @MockBean
+    StudentService studentService;
+
+    @Test
+    public void shouldSaveStudent() {
+
+    }
+
+}
